36c9311c8fc0891a805cdbf87cfe89ee7c90156b
gkechova <gkechova@yahoo.com>
2024-05-29 00:06:09 +0300

    passing judge and additional test for last symbol open bracket

--- joro_brackets_find_the_problem.py
+++ joro_brackets_find_the_problem.py
@@ -1,7 +1,5 @@
 lines = int(input())
-
 bracket_orientation = ''
-bracket_position = 0
 
 for i in range(1, lines + 1):
 
@@ -17,11 +15,8 @@ for i in range(1, lines + 1):
 
     if string == '(' and bracket_orientation == '':
             bracket_orientation = '('
-            bracket_position = i
 
     if string == ')' and bracket_orientation == '(':
-        if i > bracket_position:
-            bracket_position = i
             bracket_orientation = ''
 
 else:
