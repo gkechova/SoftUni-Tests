2872390d3d5564b11c8c4362e9c482f86d9ffb6a
gkechova <gkechova@yahoo.com>
2024-05-28 23:44:56 +0300

    version passing judge tests
    version passing judge tests but does not pass a test which is not existing in judge with input:
    5
    (
    5
    )
    +
    (

--- joro_brackets_find_the_problem.py
+++ joro_brackets_find_the_problem.py
@@ -1,48 +1,29 @@
 lines = int(input())
 
 bracket_orientation = ''
-bracket_left = 0
-bracket_right = 0
 bracket_position = 0
 
 for i in range(1, lines + 1):
 
     string = input()
 
-    if string != '(' and string != ')':
-        continue
-
     if string == ')' and bracket_orientation == '':
         print('UNBALANCED')
         break
 
-    elif string == '(' and bracket_orientation == '':
-        bracket_orientation = '('
-        bracket_position = i
-        continue
-
-    if string == ')' and bracket_orientation == '(':
-        if i == bracket_position + 2:
-            bracket_position += 2
-            bracket_orientation = ')'
-
-            continue
-
-        else:
-            print('UNBALANCED')
-            break
-
-    elif string == '(' and bracket_orientation == ')':
-        if i == bracket_position + 2:
-            bracket_position += 2
-            bracket_orientation = '('
-
-            continue
-
-        else:
-            print('UNBALANCED')
-    else:
+    if string == '(' and bracket_orientation == "(":
         print('UNBALANCED')
         break
+
+    if string == '(' and bracket_orientation == '':
+            bracket_orientation = '('
+            bracket_position = i
+
+    if string == ')' and bracket_orientation == '(':
+        if i > bracket_position:
+            bracket_position = i
+            bracket_orientation = ''
+
 else:
     print('BALANCED')
+    
